c584dd6585874d3df370928518ada1080e93ec35
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-09-04 18:06:44 +0530

    objects

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var singleNumber = function (nums) {
-  // perform XOR through the numbers.
-  // the repeated numbers will cancel out and result
-  // in the single number in the array.
-  console.log(nums.reduce((result, num) => (result ^ num, 0)));
+var car = {
+  color: "red",
+  year: "1234",
+  speeed: (a) => {
+    return `${a} is fast`;
+  },
 };
-singleNumber([2, 2, 2, 2, 4, 5, 6, 5, 6]);
+
+console.log(car.speeed("Niikiii"));
