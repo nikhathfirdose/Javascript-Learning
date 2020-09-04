a1f0d65785d5f09ebe2f19457e3a898177690b2a
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-09-04 17:28:08 +0530

    script file placement

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ var singleNumber = function (nums) {
   // perform XOR through the numbers.
   // the repeated numbers will cancel out and result
   // in the single number in the array.
-  return nums.reduce((result, num) => result ^ num, 0);
+  console.log(nums.reduce((result, num) => (result ^ num, 0)));
 };
+singleNumber([2, 2, 2, 2, 4, 5, 6, 5, 6]);
