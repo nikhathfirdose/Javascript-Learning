66fa55f6b16bbfbe1bb3f17980a1184453bb9276
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-09-16 18:25:17 +0530

    closures usage

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function Apple(z, x) {
   this.z = z;
   this.x = x;
   this.eat = function () {
-    return "eat";
+    return "eat it ";
   };
   this.throw = () => {
     return "throw";
@@ -11,3 +11,16 @@ function Apple(z, x) {
 
 var obj = new Apple("l", "p");
 var obj2 = new Apple("a", "b");
+// console.log(obj.eat());
+
+first = () => {
+  a = "first";
+  console.log(a);
+  second = () => {
+    b = "inside";
+    console.log(a + b);
+  };
+  second();
+};
+
+first();
